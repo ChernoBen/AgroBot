87761bc8f3eb2c1fb612733af77052cd8ee781b5
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-08-20 13:27:40 -0400

    fix: mongo db uri

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,6 @@
-const keys = require("./config/config");
+const keys = require("../config/config");
 const mongoose = require("mongoose");
+//"mongodb://localhost:27017/apendendoMongo",{useNewUrlParser:true,useUnifiedTopology:true}
 mongoose.connect(`mongodb://${keys.mgHost}:${keys.mgPort}/${keys.mgDatabase}`,{ useNewUrlParser: true, useUnifiedTopology: true })
 	.catch(error => {
 		console.log(error);
