5771fb197f54bdb03e8e0dfac739398399c27dc5
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-08-20 14:29:15 -0400

    setting MONGO ATLAS config

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 const keys = require("../config/config");
 const mongoose = require("mongoose");
 //"mongodb://localhost:27017/apendendoMongo",{useNewUrlParser:true,useUnifiedTopology:true}
-mongoose.connect(`mongodb://${keys.mgHost}:${keys.mgPort}/${keys.mgDatabase}`,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(keys.ATLAS_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
 	.catch(error => {
 		console.log(error);
 	});
