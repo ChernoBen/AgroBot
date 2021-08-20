4e70147f3d7b471d00f94eee7d3ef8a6378a5fe6
fang <bneto@computacao.cua.ufmt.br>
2021-08-20 02:39:06 -0400

    feat: initializing express

---
+++ server.js
@@ -0,0 +1,26 @@
+/* eslint-disable linebreak-style */
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const routes = require("./src/routes/routes");
+const keys = require(".src/config/config");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(cors());
+
+const options = {
+	customCss: ".swagger-ui .topbar { display: none }",
+	customSiteTitle: "AgroBot",
+	explorer:true
+};
+
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile,options));
+app.use(routes);
+
+app.listen(keys.apiPort,()=>{
+	console.log("Server running...");
+});
