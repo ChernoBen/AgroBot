fc34f3fbf8356accc6b2a49741dcdee3f8cf7dad
fang <bneto@computacao.cua.ufmt.br>
2021-08-20 10:00:40 -0400

    feat: add user and client routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,20 @@
 /* eslint-disable linebreak-style */
 const express = require("express");
 const router = express.Router();
+const UserController = require("../controllers/UserController");
+const ClientController = require("../controllers/ClientController");
+
+router
 
 router.use((req, res, next)=> {
 	next();
 });
+
+router.post("/register",UserController.create);
+router.auth("/auth",UserController.auth);
+
+router.post("/client",ClientController.create);
+router.get("/client/:id",ClientController.list);
+router.put("/client/:id",ClientController.update);
 
 module.exports = router;
