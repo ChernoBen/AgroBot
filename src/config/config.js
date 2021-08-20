4e5eff6cda249fef6880f3e655f46cd27fca7f21
fang <bneto@computacao.cua.ufmt.br>
2021-08-20 02:41:52 -0400

    env: env vars

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+require('dotenv/config');
+module.exports = {
+	apiPort:process.env.API_PORT,
+	secret:process.env.SECRET,
+	mgHost: process.env.HOST,
+	mgDatabase: process.env.MONGO_INITDB_DATABASE,
+	mgPort: process.env.ME_CONFIG_MONGODB_PORT
+};
