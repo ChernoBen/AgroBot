0719805cfb779265a30316e3ec77f0249c7207de
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-08-20 13:22:00 -0400

    feat:add event,client and customer endpoints

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,18 +3,23 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserController");
 const ClientController = require("../controllers/ClientController");
-
-router
+const CustomerController = require("../controllers/CostumerController");
+const EventController = require("../controllers/EventController");
 
 router.use((req, res, next)=> {
 	next();
 });
 
 router.post("/register",UserController.create);
-router.auth("/auth",UserController.auth);
+router.post("/auth",UserController.auth);
 
 router.post("/client",ClientController.create);
 router.get("/client/:id",ClientController.list);
 router.put("/client/:id",ClientController.update);
 
+router.post("/event",EventController.create);
+router.get("/event",EventController.list);
+
+router.post("/customer",CustomerController.create);
+router.get("/customer",CustomerController.list);
 module.exports = router;
