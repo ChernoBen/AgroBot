be2e31547e4857de2aa3167f15d7b238ef0e0120
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-08-20 22:54:25 -0400

    fix: wrong controller name

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,7 +12,7 @@ router.use((req, res, next)=> {
 
 router.post("/register",UserController.create);
 router.post("/auth",UserController.auth);
-router.get("/users",UserConstroler.listUsers);
+router.get("/users",UserController.listUsers);
 
 router.post("/client",ClientController.create);
 router.get("/clients",ClientController.listAll);
